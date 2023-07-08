9097ff8823afedda6c36318210d8f3dbc7174ef7
Andrew Gaytan <andrewutmail@gmail.com>
2023-07-08 13:46:08 -0500

    completed full functionality of caesar cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,86 +1,32 @@
-# goal is to take a string, and cipher the text based on stepping the index for a
-# particular value. As an example, "hello" with an change of 2 would give "jgnnq"
-test_phrase = "abcd efg hi jk LMNO pQRst uvwxyZ!a BcD, ef"
-# part A is to turn the alphabet into an array. $alpha_array is a global variable
+# The alphabet is provided to create an index for letters
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 $alpha_array = alphabet.chars()
-# alpha_array is an array of all letters in the alphabet
-
-# THESE LINES ARE TO TEST INDEXING PROCESS. DELETE THESE LATER
-#alpha_array[2] # this will return value "c", which is at index 2
-#$alpha_array.index("c") #this will return 2, the index of the value "c"
-
-def new_cipher (phrase)
-  p phrase_array = phrase.chars()
-
-end
-
-
-# part B is to index a string according to indices from alpha_array
-def string_indexer (user_string)
-  # string_array creates an array for a word, indexed_array will store indices
-  # with respect to the alphabet
-  p string_array = user_string.chars()
-  indexed_array = []
-
-  # for loop is appending indices of each letter to indexed_array
-  for letter in string_array do
-    indexed_array.push($alpha_array.index(letter))
-  end
-  indexed_array
-end
-
-# part C is to modify the indices based on a desired value
-def scrambler (user_word, step)
-  # word_array calls string_indexer and passes user_word as the argument
-  word_array = string_indexer(user_word)
-  scrambled_index = []
-
-  #for loop iterates through word_array and adds the desired step to each object
-  for value in word_array do
-    adjustment = value + step
-    scrambled_index.push(adjustment)
-  end
-  scrambled_index
-end
-
-
-# part D is to create a cipher for individual words
-def word_cipher (user_word, step)
-  # scrambled_array calls scrambler and passes user_word and step
-  scrambled_array = scrambler(user_word, step)
-  adjusted_array = []
-  new_word_array = []
-
-# This loop is to wrap index values
-  for value in scrambled_array do
-    value = value % 26
-    adjusted_array.push(value)
-    adjusted_array
-  end
-
-  # each value in adjusted_array is calling an indexed value in $alpha_array
-  # and appending the value (a letter) to new_word_array
-  for value in adjusted_array do
-    new_word_array.push($alpha_array[value])
-  end
-  new_word_array
-  new_word_array.join()
-end
-
-
-
-
 
 def caesar_cipher(phrase, step)
-  phrase_array = phrase.split
-  ciphered_array = []
+  # ciphered characters are stored in new_phrase
+  new_phrase = []
 
-  for word in phrase_array do
-    ciphered_array.push(word_cipher(word, step))
+  for character in phrase.chars do # iterate over characters in phrase variable
+
+    if character.match?(/[a-z]/) # looking for lowercase letters
+      value = $alpha_array.index(character) # creates index with respect to alphabet
+      value = (value + step) % 26 # adjusts index; modulus is needed for "wrap"
+      new_phrase.push($alpha_array[value]) # ciphered value is pushed to new_phrase
+
+    elsif character.match?(/[A-Z]/) #looking for uppercase letters
+      character.downcase! #change to a downcase;
+      value = $alpha_array.index(character) # creates index with respect to alphabet
+      value = (value + step) % 26 # adjusts index; modulus is needed for "wrap"
+      new_phrase.push($alpha_array[value].upcase) # upcase ciphered value and push
+
+    else # NOTHING ELSE IS NEEDED FOR THIS LOOP
+      new_phrase.push(character) # pushes non-letter character to new_phrase
+    end
   end
-  p ciphered_array.join(" ")
+  p "Your old phrase was: " + phrase
+  p "Your new phrase is: " + new_phrase.join
 end
 
-
+your_phrase = ""
+caesar_cipher(your_phrase, 1)
 
